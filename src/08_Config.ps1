935dd211307e7cd02446cc4738b47fe016db44f5
unknown <itm4n@gmx.com>
2021-06-22 03:04:44 +0200

    WSUS check refactoring and update

--- src/08_Config.ps1
+++ src/08_Config.ps1
@@ -50,7 +50,7 @@ function Invoke-RegistryAlwaysInstallElevatedCheck {
 function Invoke-WsusConfigCheck {
     <#
     .SYNOPSIS
-    Checks whether the WSUS is enabled and vulnerable (Wsuxploit)
+    Checks whether the WSUS is enabled and vulnerable to MitM attacks.
 
     Author: @itm4n
     License: BSD 3-Clause
@@ -61,35 +61,44 @@ function Invoke-WsusConfigCheck {
     .EXAMPLE
     PS C:\> Invoke-WsusConfigCheck
 
-    WUServer     : http://acme-upd01.corp.internal.com:8535
-    UseWUServer  : 1
+    WUServer                           : http://acme-upd01.corp.internal.com:8535
+    UseWUServer                        : 1
+    SetProxyBehaviorForUpdateDetection : 1
+
+    .NOTES
+    "Beginning with the September 2020 cumulative update, HTTP-based intranet servers will be secure by default. [...] we are no longer allowing HTTP-based intranet servers to leverage user proxy by default to detect updates." The SetProxyBehaviorForUpdateDetection value determines whether this default behavior can be overriden. The default value is 0. If it is set to 1, WSUS can use user proxy settings as a fallback if detection using system proxy fails. See links 1 and 2 below for more details.
     
     .LINK
+    https://techcommunity.microsoft.com/t5/windows-it-pro-blog/changes-to-improve-security-for-windows-devices-scanning-wsus/ba-p/1645547
+    https://docs.microsoft.com/en-us/windows/client-management/mdm/policy-csp-update#update-setproxybehaviorforupdatedetection
     https://book.hacktricks.xyz/windows/windows-local-privilege-escalation#wsus
     https://github.com/pimps/wsuxploit
+    https://github.com/GoSecure/pywsus
+    https://github.com/GoSecure/wsuspicious
     #>
 
     $WindowsUpdateRegPath = "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate"
     $WindowsUpdateAURegPath = "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
 
-    $WsusKeyServerValue = Get-ItemProperty -Path "Registry::$($WindowsUpdateRegPath)" -Name WUServer -ErrorAction SilentlyContinue -ErrorVariable ErrorGetItemProperty
-    if (-not $ErrorGetItemProperty) {
+    $WsusKeyServerValue = Get-ItemProperty -Path "Registry::$($WindowsUpdateRegPath)" -Name "WUServer" -ErrorAction SilentlyContinue -ErrorVariable ErrorGetItemProperty
+    if ($ErrorGetItemProperty) { return }
 
-        $WusUrl = $WsusKeyServerValue.WUServer
+    $UseWUServerValue = Get-ItemProperty -Path "Registry::$($WindowsUpdateAURegPath)" -Name "UseWUServer" -ErrorAction SilentlyContinue -ErrorVariable ErrorGetItemProperty
+    if ($ErrorGetItemProperty) { return }
 
-        $UseWUServerValue = Get-ItemProperty -Path "Registry::$($WindowsUpdateAURegPath)" -Name UseWUServer -ErrorAction SilentlyContinue -ErrorVariable ErrorGetItemProperty
-        if (-not $ErrorGetItemProperty) {
+    $SetProxybehaviorForUpdateDetection = Get-ItemProperty -Path "Registry::$($WindowsUpdateRegPath)" -Name "SetProxyBehaviorForUpdateDetection" -ErrorAction SilentlyContinue -ErrorVariable ErrorGetItemProperty
 
-            $WusEnabled = $UseWUServerValue.UseWUServer
-            
-            if ($WusUrl -Like "http://*" -and $WusEnabled -eq 1) {
+    $WusUrl = $WsusKeyServerValue.WUServer
+    $WusEnabled = $UseWUServerValue.UseWUServer
+    $WsusProxybehavior = $SetProxybehaviorForUpdateDetection.SetProxyBehaviorForUpdateDetection
 
-                New-Object -TypeName PSObject -Property @{
-                    WUServer    = $WusUrl
-                    UseWUServer = $WusEnabled
-                }
-            }
-        }
+    if (($WusUrl -like "http://*") -and ($WusEnabled -eq 1)) {
+
+        $Result = New-Object -TypeName PSObject
+        $Result | Add-Member -MemberType "NoteProperty" -Name "WUServer" -Value $WusUrl
+        $Result | Add-Member -MemberType "NoteProperty" -Name "UseWUServer" -Value $WusEnabled
+        $Result | Add-Member -MemberType "NoteProperty" -Name "SetProxyBehaviorForUpdateDetection" -Value $WsusProxybehavior
+        $Result
     }
 }
 
