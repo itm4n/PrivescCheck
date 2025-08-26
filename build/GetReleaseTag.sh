f332ed710b5e69d449ce96f48ba029bd1c4f6b2d
itm4n <30777390+itm4n@users.noreply.github.com>
2025-08-26 22:29:40 +0200

    Add debug to release tag script

--- build/GetReleaseTag.sh
+++ build/GetReleaseTag.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 cur_date="$(date +%Y.%m.%d)"
-cur_tag="$(gh release list --order desc --limit 1 --json tagName --jq .[].tagName | grep -s $cur_date)"
+echo "[*] Current date: ${cur_date}" >&2
+latest_release_tag="$(gh release list --order desc --limit 1 --json tagName --jq .[].tagName)"
+echo "[*] Latest release tag: ${latest_release_tag}" >&2
+echo "${latest_release_tag}" | grep -q "${cur_date}"
 if [ $? -eq 0 ]; then
-    cur_tag_arr=(${cur_tag//-/ })
-    iter=$((${cur_tag_arr[1]}+1))
+    latest_release_tag_arr=(${latest_release_tag//-/ })
+    iter=$((${latest_release_tag_arr[1]}+1))
 else
     iter=1
 fi
