2501f1f4cd61b3d7e90b5a91ed211ce08d16b409
itm4n <itm4n@gmx.com>
2026-09-02 12:41:59 +0200

    Increase tag and release history size

--- .github/workflows/clean_up_release_and_tag_history.sh
+++ .github/workflows/clean_up_release_and_tag_history.sh
@@ -2,13 +2,13 @@
 
 #
 # 1. List all releases and tags
-# 2. Select the ones older than the last 10 entries
+# 2. Select the ones older than the last 50 entries
 # 3. Iterate the list to delete them sequentially
 #
 
 last_error_code=0
 
-for t in $(gh release list --json tagName --jq .[].tagName | tail -n+11); do
+for t in $(gh release list --json tagName --jq .[].tagName | tail -n+51); do
     echo "[*] Delete release and tag: ${t}";
     if ! gh release delete "${t}" --cleanup-tag --yes; then
         last_error_code=$?
